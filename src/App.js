89a65f826c88bb53b603c7ab797d785091c4adbc
arpt004 <54246450+arpt004@users.noreply.github.com>
2022-08-11 14:15:00 +0530

    added sidebar file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import HeaderFile from './components/header/headerFile';
+import Sidebar from './components/sidebar/sidebar';
 import FooterFile from './components/footer/footer';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
           Welcome to Trilok Bhawan
           <HeaderFile />
+          <Sidebar />
           <FooterFile />
       </header>
     </div>
